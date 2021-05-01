5c43af3d03a3cad2afaec47f62f7776a0831e131
sballa1 <sballa@evoketechnologies.com>
2021-05-01 19:22:32 +0530

    my changes

--- src/dateformatting/DateToStringExample.java
+++ src/dateformatting/DateToStringExample.java
@@ -1,4 +1,4 @@
-package DateFormatting;
+package dateformatting;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
--- src/dateformatting/StringToDateExample1.java
+++ src/dateformatting/StringToDateExample1.java
@@ -1,4 +1,4 @@
-package DateFormatting;
+package dateformatting;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
